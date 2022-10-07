be4514e14934e71e3eec2cfaa8b38f13fb50efe9
luizfelipenm <111882138+luizfelipenm@users.noreply.github.com>
2022-10-07 11:10:54 -0300

    Add files via upload

---
+++ Dia da semana JS.fprg.js
@@ -0,0 +1,30 @@
+﻿function main() {
+    var dia;
+    
+    window.alert("Digite o dia da semana: ");
+    dia = Number(window.prompt('Enter a value for dia'));
+    if (dia == 1) {
+        window.alert("Domingo");
+    }
+    if (dia == 2) {
+        window.alert("Segunda-Feira");
+    }
+    if (dia == 3) {
+        window.alert("Terça-Feira");
+    }
+    if (dia == 4) {
+        window.alert("Quarta-Feira");
+    }
+    if (dia == 5) {
+        window.alert("Quinta-Feira");
+    }
+    if (dia == 6) {
+        window.alert("Sexta-Feira");
+    }
+    if (dia == 7) {
+        window.alert("Sábado");
+    }
+    if (dia < 1 || dia >= 8) {
+        window.alert("Dia inválido");
+    }
+}
